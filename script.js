388d0161f2106b2aa62fd0e97d16f4866ac4fed7
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-04-10 06:57:23 -0300

    altera imagem de fundo

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const html = document.querySelector('html')
 const buuttonFoco = document.querySelector('.app__card-button--foco')
 const buuttonCurto = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
 
 buuttonFoco.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', '/imagens/foco.png')
 })
 
 buuttonCurto.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 })
